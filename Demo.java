537674e14d8a5a6b307ca73a14a46c743ed796d5
hemant-nishad <80507644+hemant-nishad@users.noreply.github.com>
2021-03-17 16:40:15 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 class Demo {
     public static void main(String[] args) {
-        System.out.println("Demo Java code for Jenkins"); 
+        System.out.println("Demo Java code for Jenkins");
     }
 }
